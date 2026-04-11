67265cf61f45d04430a5fa899f1d7066dbdeece1
Themiya <tdeemanthe@gmail.com>
2026-04-12 00:55:40 +0530

    Refactor statusline: remove diff/fileinfo/location sections, reorganize LSP and search components

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -96,24 +96,21 @@ mini_statusline.setup({
         active = function()
             local mode, mode_hl = mini_statusline.section_mode({ trunc_width = 120 })
             local git           = mini_statusline.section_git({ trunc_width = 40 })
-            local diff          = mini_statusline.section_diff({ trunc_width = 75 })
             local diagnostics   = mini_statusline.section_diagnostics({ trunc_width = 75 })
             local lsp           = mini_statusline.section_lsp({ trunc_width = 75 })
             local filename      = mini_statusline.section_filename({ trunc_width = 140 })
-            local fileinfo      = mini_statusline.section_fileinfo({ trunc_width = 120 })
-            local location      = mini_statusline.section_location({ trunc_width = 75 })
             local search        = mini_statusline.section_searchcount({ trunc_width = 75 })
             local recording_reg = vim.fn.reg_recording()
             local recording     = recording_reg == '' and '' or 'Recording ' .. recording_reg:upper()
 
             return mini_statusline.combine_groups({
                 { hl = mode_hl,                  strings = { mode, recording } },
-                { hl = 'MiniStatuslineDevinfo',  strings = { git, diff, diagnostics, lsp } },
+                { hl = 'MiniStatuslineDevinfo',  strings = { git, diagnostics } },
                 '%<',
                 { hl = 'MiniStatuslineFilename', strings = { filename } },
                 '%=',
-                { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-                { hl = mode_hl,                  strings = { search, location } },
+                { hl = 'MiniStatuslineDevinfo',  strings = { lsp } },
+                { hl = mode_hl,                  strings = { search } },
             })
         end,
     },
